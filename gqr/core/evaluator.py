0485b72f6ef6592f54a58fe7b609326b3c2c1ffb
Lukas Galke <git@lpag.de>
2025-05-05 10:41:37 +0200

    Add score function

--- gqr/core/evaluator.py
+++ gqr/core/evaluator.py
@@ -1,4 +1,5 @@
 from typing import Union
+from collections.abc import Callable
 
 import pandas as pd
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
@@ -89,3 +90,39 @@ def evaluate_by_dataset(
 ) -> dict[str, float]:
     """Convenience function for Evaluator.evaluate_ood()"""
     return Evaluator.evaluate_ood(df=data, pred_col=pred_col, true_col=true_col, dataset_col=dataset_col)
+
+
+
+def score(model_fn: Callable[[str], int]) -> dict:
+    """ 
+    model_fn should be a callable that takes a string and returns a class label in {0, 1, 2, 3},
+    where 3 is the out-of-distribution class and 0, 1, 2 correspond to the three
+    target domains: law, finance, and health, respectively.
+    """
+    print("[GQR-Score] Loading ID test dataset...")
+    id_test_data = gqr.load_id_test_dataset()
+    print("[GQR-Score] Running model on ID test dataset...")
+    id_test_data["predictions"] = [model_fn(doc) for doc in id_test_data["text"].values]
+    id_scores = gqr.evaluate(id_test_data["predictions"], ground_truth=id_test_data["label"])
+    print("[GQR-Score] ID scores: ", id_scores)
+
+    print("[GQR-Score] Loading ID test dataset...")
+    ood_test_data = gqr.load_ood_test_dataset()
+    print("[GQR-Score] Running model on OOD test dataset...")
+    ood_test_data["predictions"] = [model_fn(doc) for doc in ood_test_data["text"].values]
+    ood_scores_df = gqr.evaluate_by_dataset(ood_test_data, pred_col="predictions", true_col="label", dataset_col="dataset")
+    print("[GQR-Score] OOD scores:", ood_scores_df, sep='\n')
+
+    id_accuracy = id_scores["accuracy"]
+    mean_ood_accuracy = ood_scores_df['accuracy'].mean()
+
+    gqr_score =  2 * (id_accuracy * mean_ood_accuracy) / (id_accuracy + mean_ood_accuracy)
+
+    scores = {
+        "id_accuracy": id_accuracy,
+        "ood_accuracy": mean_ood_accuracy,
+        "gqr_score": gqr_score,
+    }
+    print("[GQR-Score] Final scores: ", scores)
+
+    return scores
